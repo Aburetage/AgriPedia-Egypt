1e554010b1c0332e17c07aa750a79039d16a7cf2
Aburetage <aliazmy30@gmail.com>
2026-06-30 16:52:09 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'agripedia-v38';
+const CACHE_NAME = 'agripedia-v39';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
-  './css/style.css?v=27',
-  './js/app.js?v=32',
+  './css/style.css?v=28',
+  './js/app.js?v=33',
   './manifest.json',
   './data/ar/index.json',
   './data/ar/glossary.json',
